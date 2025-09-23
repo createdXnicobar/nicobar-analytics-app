cd64b82b4279000123f1ad5630e08cceba0b072b
Harshit Rai Verma <harshit.rai.verma@gmail.com>
2025-09-23 09:48:13 +0530

    add scripts for pushing image to ECR, creating Apprunner service and deploying a new version
    - push-to-ecr works
    - health check config issues in create-apprunner-service

---
+++ backend/deployment/push-to-ecr.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# TODO: set more verbose error boundaries
+set -e
+
+AWS_REGION="ap-south-1"
+AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output text)
+REPO_NAME="created-nicobar/nicobar-analytics-project"
+IMAGE_TAG=${1:-latest}
+
+ECR_URI="${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${REPO_NAME}:${IMAGE_TAG}"
+
+# Build Docker image
+docker build -f ../Dockerfile -t ${REPO_NAME}:${IMAGE_TAG} ..
+
+
+docker tag ${REPO_NAME}:${IMAGE_TAG} ${ECR_URI}
+
+
+aws ecr get-login-password --region ${AWS_REGION} | docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com
+
+
+aws ecr describe-repositories --repository-names ${REPO_NAME} --region ${AWS_REGION} >/dev/null 2>&1 || \
+aws ecr create-repository --repository-name ${REPO_NAME} --region ${AWS_REGION}
+
+
+# Push image
+docker push ${ECR_URI}
+
+echo "Image pushed to: ${ECR_URI}"
