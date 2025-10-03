5ff0152c133dd80afc13216a7858630296b0b318
Harshit Rai Verma <harshit.rai.verma@gmail.com>
2025-10-03 19:34:38 +0530

    add dockerignore file and update names in push-to-ecr script

--- backend/deployment/push-to-ecr.sh
+++ backend/deployment/push-to-ecr.sh
@@ -3,12 +3,13 @@
 # TODO: set more verbose error boundaries
 set -e
 
-AWS_REGION="ap-south-1"
+AWS_REGION="ap-southeast-2"
 AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output text)
-REPO_NAME="created-nicobar/nicobar-analytics-project"
+REPO_NAME="nicobar-backend"
 IMAGE_TAG=${1:-latest}
 
 ECR_URI="${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${REPO_NAME}:${IMAGE_TAG}"
+# 317821645521.dkr.ecr.ap-southeast-2.amazonaws.com/nicobar-backend
 
 # Build Docker image
 docker build --platform linux/amd64 -f ../Dockerfile -t ${REPO_NAME}:${IMAGE_TAG} ..
@@ -20,9 +21,9 @@ docker tag ${REPO_NAME}:${IMAGE_TAG} ${ECR_URI}
 aws ecr get-login-password --region ${AWS_REGION} | docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com
 
 
+# create repo by this name if doesn't exist already
 aws ecr describe-repositories --repository-names ${REPO_NAME} --region ${AWS_REGION} >/dev/null 2>&1 || \
 aws ecr create-repository --repository-name ${REPO_NAME} --region ${AWS_REGION}
-
 
 # Push image
 docker push ${ECR_URI}
